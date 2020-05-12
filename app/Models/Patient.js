c294e94d3e7ad02cb87be43f2a16dc3aed7c062f
Lucas <lucasmag97@gmail.com>
2020-05-12 02:13:14 -0300

    Patient renaming

--- app/Models/Patient.js
+++ app/Models/Patient.js
@@ -3,7 +3,7 @@
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
-class PatientSchema extends Model {
+class Patient extends Model {
 }
 
-module.exports = PatientSchema
+module.exports = Patient
